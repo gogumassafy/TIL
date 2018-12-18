a43b8beca96debb425e9f3b37a9c81aadcca0305
gogumassafy <gogumassafy@gmail.com>
2018-12-18 17:47:36 +0900

    second_day_last_push

--- file_rw/make_txt.py
+++ file_rw/make_txt.py
@@ -1,8 +1,9 @@
-# with open("test.txt", "w", encoding = "utf-8") as f:
-#     for i in range(1,6):
-#         f.write(f"{i}번째 줄입니다. \t")
+# f = open("test.txt", "w", encoding = "utf-8")
+# for i in range(1,6):
+#     f.write(f"{i}번째 줄입니다. \t")
+# f.close()
 
-f = open("test.txt", "w", encoding = "utf-8")
-for i in range(1,6):
-    f.write(f"{i}번째 줄입니다. \t")
-f.close()
+with open("new.txt", "r", encoding = "utf-8") as f:
+    lines = f.readlines()
+    for line in lines:
+        print(line)
