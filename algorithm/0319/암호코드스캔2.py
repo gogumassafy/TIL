3ce99d685b97e9330ef7058485819114dae576d5
gogumassafy <gogumassafy@gmail.com>
2019-03-20 23:06:10 +0900

    0320 algorithm

--- algorithm/0319/암호코드스캔2.py
+++ algorithm/0319/암호코드스캔2.py
@@ -2,14 +2,13 @@ import sys
 sys.stdin = open('암호코드스캔.txt')
 
 
-def check(r):
-    first = raw[r].index(1)
+def check(r, c):
     first_num = 1
     row_idx = 0
     col_idx = 0
     cnt = 0
     quotient = 0
-    for i in range(first, len(raw[r])):
+    for i in range(c, len(raw[r])):
         if row_idx == 7 and col_idx == 3:
             num_count[row_idx][col_idx] = 7 * quotient - sum(num_count[row_idx])
             break
@@ -35,8 +34,8 @@ def check(r):
     col = 0
     for i in range(len(num_count)):
         col += sum(num_count[i])
-    row = countrow(r, first)
-    clear(r, first, row, col)
+    row = countrow(r, c)
+    clear(r, c, row, col)
 
     if quotient:
         for i in range(len(num_count)):
@@ -50,11 +49,16 @@ def check(r):
                 break
         else:
             num_count[i] = -float('inf')
+    sum_num = sum(num_count)
+    for i in range(len(num_count)):
+        if i % 2:
+            sum_num += num_count[i] * 2
+    return 0 if sum_num % 10 else sum(num_count)
 
 
 def countrow(r, c):
     count = 0
-    while raw[r][c] != '0':
+    while raw[r][c]:
         count += 1
         r += 1
     return count
@@ -63,12 +67,12 @@ def countrow(r, c):
 def clear(r, c, rowcount, colcount):
     global N, M
     for i in range(r, r+rowcount):
-        if i >= N:
+        if i >= len(raw):
             break
         for j in range(c, c+colcount):
-            if j >= M:
+            if j >= len(raw[0]):
                 break
-            raw[i][j] = '0'
+            raw[i][j] = 0
 
 
 pwd = [
@@ -112,26 +116,26 @@ for i in range(16):
 T = int(input())
 for tc in range(1, T + 1):
     N, M = map(int, input().split())
-    raw = [list(reversed(input())) for _ in range(N)]
-    sum_nums = 0
+    raw = [input() for _ in range(N)]
+    raw = [list(reversed(raw[i])) for i in range(len(raw))
+           if raw[i].count('0') < M or (not raw[i].count('0') < M and raw[i - 1].count('0') < M)]
+
     for i in range(len(raw)):
         temp = []
         for j in range(M):
-            if raw[i][i].isalpha():
+            if raw[i][j].isalpha():
                 idx = ord(raw[i][j]) - ord('A') + 10
             else:
                 idx = int(raw[i][j])
             temp += num[idx]
         raw[i] = temp
+    sum_nums = 0
     for i in range(len(raw)):
-        num_count = [[0] * 4 for _ in range(8)]
-        col = check(i)
-
-
-
-
-
-
+        for j in range(len(raw[0])):
+            if raw[i][j]:
+                num_count = [[0] * 4 for _ in range(8)]
+                each = check(i, j)
+                sum_nums += each
     print('#{} {}'.format(tc, sum_nums))
 
 #1 38
