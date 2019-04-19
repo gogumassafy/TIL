a73c986486623905841aea1c32d8eb2fff540f6a
gogumassafy <gogumassafy@gmail.com>
2019-04-19 20:00:11 +0900

    sort

--- algorithm/0419/퀵정렬.py
+++ algorithm/0419/퀵정렬.py
@@ -1,4 +1,8 @@
-def quicksort():
-    a[0] = 0
+def quicksort(s, e):
+    pivot = (s + e) // 2
+    quicksort(s, pivot)
+    quicksort(pivot + 1, e)
+
 
 a = [1, 3, 49, 4, 69, 2, 1, 100, 59, 43]
+quicksort(0, len(a) - 1)
