1bd0dfebf5a380f76d465499bfa23dd74f45d6a7
gogumassafy <gogumassafy@gmail.com>
2019-06-20 00:06:24 +0900

    0620 algorithm

--- algorithm/0619/감시.py
+++ algorithm/0619/감시.py
@@ -1,4 +1,7 @@
 # 상, 좌, 하, 우
+dr = [-1, 0, 1, 0]
+dc = [0, -1, 0, 1]
+
 cctv = [
     [0, 0, 0, 0],
     [0, 0, 0, 1],
@@ -9,19 +12,64 @@ cctv = [
 ]
 
 
-def dfs(N, depth, sum):
+def dfs(N, depth, total):
     global result
-    if depth == N:
-        result = min(result, sum)
+    if result == 0:
         return
+    if depth == N:
+        result = min(result, total)
+        return
+    cctvNum = raw[stack[depth][0]][stack[depth][1]]
     for i in range(4):
-        num = check()
-        depth(N, depth + 1,sum - num)
+        newDirection = cctv[cctvNum][i + 1:4] + cctv[cctvNum][0:i + 1]
+        num = check(depth, newDirection)
+        dfs(N, depth + 1, total - num)
+        clear(depth, newDirection)
+        if cctvNum == 5:
+            break
 
 
+def check(depth, direction):
+    global N, M
+    count = 0
+    for i in range(4):
+        if not direction[i]:
+            continue
+        r, c = stack[depth]
+        while 1:
+            r = r + dr[i]
+            c = c + dc[i]
+            if not (N > r >= 0 and M > c >= 0):
+                break
+            if raw[r][c] == 6:
+                break
+            if raw[r][c] == 0:
+                raw[r][c] = 10
+            elif raw[r][c] < 6:
+                continue
+            else:
+                raw[r][c] += 1
+                continue
+            count += 1
+    return count
 
-def check():
 
+def clear(depth, direction):
+    for i in range(4):
+        if not direction[i]:
+            continue
+        r, c = stack[depth]
+        while 1:
+            r = r + dr[i]
+            c = c + dc[i]
+            if not (N > r >= 0 and M > c >= 0):
+                break
+            if raw[r][c] == 6:
+                break
+            if raw[r][c] == 10:
+                raw[r][c] = 0
+            elif raw[r][c] > 10:
+                raw[r][c] -= 1
 
 
 N, M = map(int, input().split())
