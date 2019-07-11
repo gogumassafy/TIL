023e9d269766cb96448eab2eec7220db2fa98efe
JO SEONG-WON <gogumassafy@gmail.com>
2019-07-11 20:00:56 +0900

    0711 algorithm

--- algorithm/c++/algorithm/퀴즈/퀴즈.cpp
+++ algorithm/c++/algorithm/퀴즈/퀴즈.cpp
@@ -1,13 +1,35 @@
-#include <stdio.h>
+#include <iostream>
+#include <cmath>
+using namespace std;
 
 int T, N;
+long long arr[1000001] = { 0, }, temp;
+
+
+long long int power(int number, int up) {
+	long long result = 1;
+	if (up == 1) {
+		return number;
+	}
+	else {
+		result = (power(number, up / 2) % 1000000007);
+		result *= result;
+		if (up % 2)
+			result *= power(number, 1);
+	}
+	return result % 1000000007;
+}
 
 int main() {
+	arr[1] = 1;
+	for (int i = 2; i < 1000001; ++i) {
+		arr[i] = arr[i - 1] + power(i, i);
+		arr[i] %= 1000000007;
+	}
 	scanf("%d", &T);
 	for (int tc = 1; tc <= T; ++tc) {
 		scanf("%d", &N);
-		int temp = N * (1 + N) / 2;
-		printf("#%d %d\n", tc, temp);
+		printf("#%d %lld\n", tc, arr[N]);
 	}
 	return 0;
 }
