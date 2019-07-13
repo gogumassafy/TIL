6554396d0dd4e876cd7d002f45d8862900a09a1a
gogumassafy <gogumassafy@gmail.com>
2019-07-13 23:48:01 +0900

    0713 algorihtm

--- algorithm/c++/algorithm/운동/운동.cpp
+++ algorithm/c++/algorithm/운동/운동.cpp
@@ -4,45 +4,34 @@ using namespace std;
 
 int T, N, M, s, e, c, result, arr[401][401], visit[401] = { 0, }, start;
 
-
-void run(int s, int total);
-
-
 int main() {
 	scanf("%d", &T);
 	for (int tc = 1; tc <= T; ++tc) {
 		scanf("%d %d", &N, &M);
-		memset(arr, 0, 401);
+		memset(arr, 20000, sizeof(arr));
+
 		result = 987654321;
 		for (int m = 0; m < M; ++m) {
 			scanf("%d %d %d", &s, &e, &c);
 			arr[s][e] = c;
 		}
 		for (int i = 1; i < N + 1; ++i) {
-			start = i;
-			run(i, 0);
+			for (int j = 1; j < N + 1; ++j) {
+				for (int k = 1; k < N + 1; ++k) {
+					if (arr[j][k] > arr[j][i] + arr[i][k]) {
+						arr[j][k] = arr[j][i] + arr[i][k];
+					}
+				}
+			}
+		}
+		for (int i = 1; i < N + 1; ++i) {
+			if (result > arr[i][i]) {
+				result = arr[i][i];
+			}
 		}
 		if (result == 987654321)
 			result = -1;
 		printf("#%d %d\n", tc, result);
 	}
 	return 0;
-}
-
-void run(int s, int total) {
-	if (total >= result) {
-		return;
-	}
-	if (total && s == start) {
-		result = total;
-	}
-	for (int i = 1; i < N + 1; ++i) {
-		if (arr[s][i] == 0)
-			continue;
-		if (visit[i])
-			continue;
-		visit[i] = 1;
-		run(i, total + arr[s][i]);
-		visit[i] = 0;
-	}
 }
