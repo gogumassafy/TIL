933db929cf513b4529211f42eeb7d93ae68f287c
JO SEONG-WON <gogumassafy@gmail.com>
2019-08-01 20:00:47 +0900

    0801 algorithm

--- algorithm/c++/algorithm/영이는영이시져시져!/영이는영이시져시져!.cpp
+++ algorithm/c++/algorithm/영이는영이시져시져!/영이는영이시져시져!.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int T, N, dr[2] = { 1, 0 }, dc[2] = { 0, 1 }, arr[1000][1000], result, countZero;
@@ -8,22 +9,29 @@ void dfs(int sr, int sc, long long total);
 int main() {
 	scanf("%d", &T);
 	for (int tc = 1; tc < T + 1; ++tc) {
+		result = 987654321;
 		scanf("%d", &N);
 		for (int i = 0; i < N; ++i) {
 			for (int j = 0; j < N; ++j) {
 				scanf("%d", &arr[i][j]);
 			}
 		}
-		dfs(0, 0, 0);
+		dfs(0, 0, arr[0][0]);
 		printf("#%d %d\n", tc, result);
 	}
 	return 0;
 }
 
 void dfs(int sr, int sc, long long total) {
+	if (result != 987654321) {
+		int limit = pow(10, result);
+		if (total % limit == 0) {
+			return;
+		}
+	} 
 	if (sr == N - 1 && sc == N - 1) {
 		countZero = 0;
-		while (!total % 10) {
+		while (!(total % 10)) {
 			countZero++;
 			total /= 10;
 		}
@@ -32,14 +40,17 @@ void dfs(int sr, int sc, long long total) {
 		}
 		return;
 	}
-	int nr, int nc;
+	int nr, nc;
 	for (int i = 0; i < 2; ++i) {
 		nr = sr + dr[i];
 		nc = sc + dc[i];
 		if (nr >= N || nc >= N) {
 			continue;
 		}
-		dfs(nr, nc, total * arr[nr][nc]);
+		if (arr[nr][nc] == 0) {
+			continue;
+		}
+		dfs(nr, nc, total * (long long) arr[nr][nc]);
 	}
 	return;
 }
