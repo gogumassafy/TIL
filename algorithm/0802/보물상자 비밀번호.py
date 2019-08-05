5e57fbfd69365f35ab2b16fc2d4f3c6a1827e04a
JO SEONG-WON <gogumassafy@gmail.com>
2019-08-05 20:00:38 +0900

    0805 algorithm

--- algorithm/0802/보물상자 비밀번호.py
+++ algorithm/0802/보물상자 비밀번호.py
@@ -1,6 +1,3 @@
-import sys
-sys.stdin = open('보물상자 비밀번호.txt')
-
 T = int(input())
 for tc in range(1, T + 1):
     N, K = map(int, input().split())
@@ -8,5 +5,30 @@ for tc in range(1, T + 1):
     head = 0
     result = []
     each_part = len(input_string) // 4
-    for i in range(each_part):
-        for j in range(4):
+    for s in range(each_part):
+        for i in range(4):
+            each_sum = 0
+            for j in range(each_part):
+                idx = s + i * each_part + j
+                if idx >= N:
+                    idx = idx - N
+                temp = input_string[idx]
+                if temp == 'A':
+                    temp = 10
+                elif temp == 'B':
+                    temp = 11
+                elif temp == 'C':
+                    temp = 12
+                elif temp == 'D':
+                    temp = 13
+                elif temp == 'E':
+                    temp = 14
+                elif temp == 'F':
+                    temp = 15
+                else:
+                    temp = int(temp)
+                each_sum += temp * 16**(each_part - 1 - j)
+            result.append(each_sum)
+    result = list(set(result))
+    result.sort(reverse=True)
+    print('#{} {}'.format(tc, result[K - 1]))
