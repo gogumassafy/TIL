279cd8d4194d7119c68eec5363cb5c887892c3f5
JO SEONG-WON <gogumassafy@gmail.com>
2019-08-29 18:01:02 +0900

    algorithm

---
+++ algorithm/c++/algorithm/덕환이의카드뽑기/덕환이의카드뽑기.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+using namespace std;
+
+int T, result, N, K;
+long long arr[]
+
+int main() {
+	scanf("%d", &T);
+	for (int tc = 1; tc <= T; ++tc) {
+		scanf("%d %d", &N, &K);
+		
+
+		printf("#%d %d", tc, result);
+	}
+
+}
