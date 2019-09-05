964c41e6d7f8581a188c18ecf735900f08a122d6
JO SEONG-WON <gogumassafy@gmail.com>
2019-09-05 19:57:20 +0900

    algorithm

---
+++ algorithm/c++/algorithm/인터벌커버/인터벌커브.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+long long T, X, N;
+
+int main() {
+	scanf("%ld", &T);
+	for (int tc = 1; tc <= T; ++tc) {
+		scanf("%ld %ld", &X, &N);
+
+	}
+	return 0;
+}
