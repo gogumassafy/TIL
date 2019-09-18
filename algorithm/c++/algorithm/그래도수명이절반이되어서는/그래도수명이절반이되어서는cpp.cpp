105b0af554fff38eaf92408dbe9f60085a5b6377
JO SEONG-WON <gogumassafy@gmail.com>
2019-09-18 20:00:29 +0900

    algorithm

--- algorithm/c++/algorithm/그래도수명이절반이되어서는/그래도수명이절반이되어서는cpp.cpp
+++ algorithm/c++/algorithm/그래도수명이절반이되어서는/그래도수명이절반이되어서는cpp.cpp
@@ -1,38 +1,65 @@
 #include <iostream>
 using namespace std;
 
-int T, N, K, W[200000], visited[200000] = { 0, }, wearlevel;
+int T, N, K, W[200000], S[200000] = { 0, }, wearlevel, maxNum, minNum, ans;
 
 bool check(int w) {
-
+	int count = 0, point = 0;
+	for (int i = 0; i < N; ++i) {
+		if (W[i] > w) {
+			count = 0;
+			continue;
+		}
+		++count;
+		if (count == S[point]) {
+			++point;
+			count = 0;
+			if (point == K) {
+				return true;
+			}
+		}
+	}
+	return false;
 }
 
 void binary_search(int left, int right) {
 	int mid;
-
-	while (left < right) {
+	ans = left;
+	while (left <= right) {
 		mid = (left + right) / 2;
 		// 해당 답이 몇인지 고려
 		
 		if (check(mid)) {
-			left = mid;
+			right = mid - 1;
+			ans = mid;
 		}
 		else {
-			right = mid;
+			left = mid + 1;
 		}
 	}
-
 }
 
 int main() {
 	scanf("%d", &T);
 	for (int tc = 1; tc <= T; ++tc) {
 		scanf("%d %d", &N, &K);
+		minNum = 200000;
+		maxNum = 1;
 		for (int i = 0; i < N; ++i) {
 			scanf("%d", &W[i]);
+			if (W[i] > maxNum) {
+				maxNum = W[i];
+			}
+			if (W[i] < minNum) {
+				minNum = W[i];
+			}
 		}
-		binary_search(0, N);
-		printf("#%d %d", tc, wearlevel);
+		for (int i = 0; i < K; ++i) {
+			scanf("%d", &S[i]);
+		}
+
+		binary_search(0, maxNum);
+		printf("#%d %d\n", tc, ans);
 	}
 	return 0;
 }
--- algorithm/c++/algorithm/평등주의/평등주의.cpp
+++ algorithm/c++/algorithm/평등주의/평등주의.cpp
@@ -5,8 +5,13 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int T;
 
+int main() {
+	scanf("%d", &T);
+	for (int tc = 1; tc <= T; ++tc) {
+
+	}
 	return 0;
 }
 
