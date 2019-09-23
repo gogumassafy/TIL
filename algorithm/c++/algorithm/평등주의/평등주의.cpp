d1af18ebe40a4433b22450292a81c887444f2c4d
JO SEONG-WON <gogumassafy@gmail.com>
2019-09-23 19:34:25 +0900

    algorithm

--- algorithm/c++/algorithm/평등주의/평등주의.cpp
+++ algorithm/c++/algorithm/평등주의/평등주의.cpp
@@ -5,10 +5,73 @@
 #include <iostream>
 using namespace std;
 
-int T, N, K, arr[100000], ans;
+int T, N, K, arr[100000], ans, length[100000], length2[100000];
 
 bool check(int W) {
+	int count = K;
+	for (int i = 0; i < N; ++i) {
+		int min = 987654321;
+		for (int j = 0; j < N; ++j) {
+			int temp = W * (i - j);
+			if (temp < 0) {
+				temp *= -1;
+			}
+			temp += arr[j];
+			if (temp < min) {
+				min = temp;
+			}
+		}
+		count -= (arr[i] - min);
+		if (count < 0) {
+			return false;
+		}
+		length[i] = min;
+	}
+	return true;
+}
 
+bool check2(int W) {
+	int count = K;
+	int min;
+	for (int i = 0; i < N; ++i) {
+		min = 987654321;
+		if (i == 0) {
+			min = arr[0];
+		}
+		else {
+			min = length[i - 1] + W;
+			if (min > arr[i]) {
+				min = arr[i];
+			}
+		}
+		length[i] = min;
+	}
+
+	for (int i = N - 1; i >= 0; --i) {
+		min = 987654321;
+		if (i == N - 1) {
+			min = arr[N - 1];
+		}
+		else {
+			min = length2[i + 1] + W;
+			if (min > arr[i]) {
+				min = arr[i];
+			}
+		}
+		length2[i] = min;
+	}
+
+	for (int i = 0; i < N; ++i) {
+		min = length[i];
+		if (min > length2[i]) {
+			min = length2[i];
+		}
+		count -= arr[i] - min;
+		if (count < 0) {
+			return false;
+		}
+	}
+	return true;
 }
 
 void binarySearch(int left, int right) {
@@ -16,7 +79,7 @@ void binarySearch(int left, int right) {
 	ans = left;
 	while (left <= right) {
 		mid = (left + right) / 2;
-		if (check(mid)) {
+		if (check2(mid)) {
 			ans = mid;
 			right = mid - 1;
 		}
@@ -34,6 +97,7 @@ int main() {
 		for (int i = 0; i < N; ++i) {
 			scanf("%d", &arr[i]);
 		}
+		binarySearch(0, 1000000000);
 		printf("#%d %d\n", tc, ans);
 	}
 	return 0;
