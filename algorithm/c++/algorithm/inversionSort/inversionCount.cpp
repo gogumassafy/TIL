457107d27df609a40a55062108a8de069d83a022
gogumassfay <gogumassafy@gmail.com>
2020-01-14 23:29:16 +0900

    inversion couting

--- algorithm/c++/algorithm/inversionSort/inversionCount.cpp
+++ algorithm/c++/algorithm/inversionSort/inversionCount.cpp
@@ -1,42 +1,46 @@
 #include <iostream>
 using namespace std;
 
-int T, N, input[100000], sort[100000], ans;
+int T, N;
+long long input[1000000], sort[1000000], ans;
 
-int dq(int start, int end);
+long long dq(int start, int end);
 
 int main() {
-	scanf("%d", &T);
-	for (int tc = 1; tc <= T; ++tc) {
-		ans = 0;
-		scanf("%d", &N);
-		for (int i = 0; i < N; ++i) {
-			scanf("%d", &input[i]);
-			sort[i] = input[i];
-		}
-
-		ans = dq(0, N - 1);
-		printf("#%d %d", tc, ans);
+	ans = 0;
+	scanf("%d", &N);
+	for (int i = 0; i < N; ++i) {
+		scanf("%lld", &input[i]);
+		sort[i] = input[i];
 	}
+
+	ans = dq(0, N - 1);
+	printf("%lld\n", ans);
+
 	return 0;
 }
 
-int dq(int start, int end) {
-	int mid = (start + end) / 2, left = start, right = mid + 1, value = 0, temp = 0, counting = start;
+long long dq(int start, int end) {
+	long long mid = (start + end) / 2, left = start, right = mid + 1, value = 0, temp = 0, counting = start;
 	if (start == end) return 0;
 	value = dq(start, mid) + dq(mid + 1, end);
 	while (left <= mid && right <= end) {
-		if (input[left] <= input[right]) {
-			// 새로운 배열에 뽑아주고 left 1증가
-			sort[counting++] = input[left];
+		if (input[left] < input[right]) {
+			sort[counting++] = input[left++];
 			value += temp;
-
 		}
 		else {
-			// 새로운 배열에 뽑아주고 right 1증가
-			sort[counting++] = input[right];
+			sort[counting++] = input[right++];
 			++temp;
 		}
+	}
+
+	while (left <= mid) {
+		sort[counting++] = input[left++];
+		value += temp;
+	}
+	while (right <= end) {
+		sort[counting++] = input[right++];
 	}
 
 	for (int i = start; i <= end; ++i) {
